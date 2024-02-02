2c5f2ac00f3f9bbe7b68ec44172cacf6ea553b3c
LoweComp <terci_300@outlook.com>
2024-02-01 21:36:51 -0300

    User Seed & Factory 2.0

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,22 +13,57 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
+
     public function run(): void
     {
+
+        $estates = ['Acre',
+            'Alagoas',
+            'Amapá',
+            'Amazonas',
+            'Bahia',
+            'Ceará',
+            'EspíritO Santo',
+            'Goiás',
+            'Maranhão',
+            'Mato Grosso',
+            'Mato Grosso do Sul',
+            'Minas Gerais',
+            'Pará',
+            'Paraíba',
+            'Paraná',
+            'Pernambuco',
+            'Piauí',
+            'Rio de Janeiro',
+            'Rio Grande do Norte',
+            'Rio Grande do Sul',
+            'Rondônia',
+            'Roraima',
+            'Santa Catarina',
+            'São Paulo',
+            'Sergipe',
+            'Tocantins',
+            'Distrito Federal'];
+
         User::create([
             'name' => 'Ronaldo',
             'email' => 'adm@hospital.com.br',
             'email_verified_at' => now(),
-            'password' => Hash::make('alprazolan'), // password
+            'password' => Hash::make('12345678'), // password
             'remember_token' => Str::random(10),
             'birth_date' => fake()->date('Y-m-d'),
-            'address' => fake()->address(),
-            'phone' => fake()->phoneNumber(),
-            'CPF' => '08612771200',
+            'zip_code' => fake()->postcode(),
+            'estate' => fake()->randomElement($estates),
+            'city' => fake()->city(),
+            'street' => fake()->streetName(),
+            'number' => fake()->randomNumber(4, false),
+            'complement' => 'ap 401',
+            'phone' => fake()->numberBetween(),
+            'cpf' => '08612771200',
             'user_type' => 'adm'
         ]);
 
-        User::factory()->count(20)->create();
+        User::factory()->count(50)->create();
 
     }
 
